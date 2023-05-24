5ab305f61ea32ac6ba908b8955c8331beb1b4d91
javadjahangiri <javad.jahangiri.niopdc@gmail.com>
2023-05-24 10:40:54 +0330

    Add project files.

---
+++ WebApplication1/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
