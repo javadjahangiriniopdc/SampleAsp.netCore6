8141d8995b9ffd2477d1c28d1b3a19d69c432f7d
javadjahangiri <javad.jahangiri.niopdc@gmail.com>
2023-05-25 11:55:14 +0330

    fix project

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<PersonContext>( c => c.UseSqlServer("server=.;Initial Catalog=Intero;User Id=sa;Password=11qaz!QAZ");
+builder.Services.AddDbContext<PersonContext>( c => c.UseSqlServer("server=.;Initial Catalog=Intero;User Id=sa;Password=1qaz!QAZ;"));
 
 var app = builder.Build();
 
